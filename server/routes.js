939bcd1a58bbf3116122be83660d887c7f858029
“Jason <jasonveng@outlook.com>
2016-03-24 20:26:36 +1300

    remove .DS_Store

--- server/routes.js
+++ server/routes.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
   app.get('/', function (req, res) {
     res.end('Hello there, whats up? Hello. Hi hi')
   })
+  app.get('/test', function (req, res) {
+    res.end('elon musk')
+  })
 }
