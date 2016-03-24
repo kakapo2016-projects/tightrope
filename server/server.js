cefc320cfbd66b08b40631821cfe2e030e90d660
Nick Maskell <nrmaskell@gmail.com>
2016-03-24 17:02:04 +1300

    Add base passport setup and modular routes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,50 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+// importing routes
+require('./routes')(app);
 
+
+// passport authentication
+var app = express()
+app.use(require('serve-static')(__dirname + '../public'))
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err) }
+      if (!user) { return done(null, false) }
+      if (!user.verifyPassword(password)) { return done(null, false) }
+      return done(null, user)
+    })
+  }
+))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user)
+  })
+})
+
+// passport route
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/')
+  })
+
+
+// listener
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
