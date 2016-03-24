cd267ac2fafa95c06efe46bb557fe3d1e78876d7
Ashley <Ashleystanbridge@icloul.com>
2016-03-25 11:42:28 +1300

    Wip

--- src/components/Accolades.js
+++ src/components/Accolades.js
@@ -1,21 +1,20 @@
 import React from 'react'
 
 export default React.createClass({
-
   render: function () {
+    let Accolade = ['32', '45', '1', '19']
     return (
       <div>
+        <h3>Accolades</h3>
+        <ul className='badges'>
+          <li>img</li>
+          <li>img</li>
+          <li>img</li>
+          <li>img</li>
+        </ul>
         <div>
-          <p>Active Streak: </p>
-        </div>
-        <div>
-          <p>Badges: </p>
-        </div>
-        <div>
-          <p>Credits: </p>
-        </div>
-        <div>
-          <p>Fans: </p>
+          <h3>Active:{this.props.active}</h3>
+          <h3>Credits:{this.props.credits}</h3>
         </div>
       </div>
     )
--- test/components/accolades-test.js
+++ test/components/accolades-test.js
@@ -6,8 +6,21 @@ import React from 'react'
 import Accolades from '../../src/components/Accolades'
 
 describe('Accolades', () => {
+  var data = {[
+    accolades: {activeStreak: 6},
+    {badges: ['one year', 'nine years', 'Nilu']},
+    {credits: 10}
+  ]}
   xit('should have a div', () => {
     const wrapper = shallow(<Accolades />)
-    expect(wrapper.find('div')).to.have.length(1)
+    expect(wrapper.find('div')).to.have.length(2)
+  })
+  it('should render 4 li', () => {
+    const wrapper = mount(<Accolades badges={['Ashley', 'Ashley', 'Ashley', 'Ashley']} />)
+    expect(wrapper.find('li')).to.have.length(4)
+  })
+  xit('should have a div', () => {
+    const wrapper = shallow(<Accolades />)
+    expect(wrapper.find('div')).to.have.length(2)
   })
 })
