593912b5d6b2a1b0d9eb06da153beac8fbdea64c
Howard Smith <howardsmithnz@gmail.com>
2016-03-24 20:25:03 +1300

    minor change to routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,11 @@
 module.exports = function (app) {
+  // ----- initial test routes ----- //
+
   app.get('/', function (req, res) {
-    res.end('Hello there, whats up? Hello. Hi hi')
+    res.send('Hello there, whats up? Hello. Hi hi')
+  })
+
+  app.get('/test', function (req, res) {
+    res.send('Test. Check...1...2...')
   })
 }
