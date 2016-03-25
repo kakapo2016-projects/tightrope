35ea507b80b2d638135dbe4ce18c55c344d8739c
Jason Veng <jasonveng@outlook.com>
2016-03-25 14:19:27 +1300

    change static data to nick json spec

--- server/routes.js
+++ server/routes.js
@@ -5,10 +5,14 @@ module.exports = function (app, cors, corsOptions) {
   // ----- dummy data for testing ----- //
 
   const dummy = {
-    username: 'simon lenovo tegg',
-    profile_pic: 'http://rack.2.mshcdn.com/media/ZgkyMDE1LzEwLzE2Lzk4L2NhdG1vdXRoLmQyOWFiLnBuZwpwCXRodW1iCTk1MHg1MzQjCmUJanBn/ee79f012/f77/cat-mouth.jpg',
-    accolades: [],
-    all_user_photos: []
+    'username': 'Simon Teg',
+    'profilepic': 'url1',
+    'photoset': ['url1', 'url2'],
+    'accolades': [{
+      'credits': 10,
+      'badges': ['one year', 'nine years', 'Nilu'],
+      'activeStreak': 6
+    }]
   }
 
   // ----- set up DB ----- //
