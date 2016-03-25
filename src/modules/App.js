6374b8069ae66ba4627c31d224295698f95a38b4
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-03-26 12:40:44 +1300

    uploads squares only

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import Feed from './Feed'
 import Navbar from './Navbar'
-import { Grid, Row } from 'react-bootstrap'
-import request from 'superagent'
-
-var Ω = require('lomega')
+import { Grid } from 'react-bootstrap'
 
 export default React.createClass({
-
-  uploadPhoto: function (file, public_id) {
-
-    const API_ROOT = 'https://api.cloudinary.com/v1_1/dvzbt8kfq/image/upload'
-    const serverroot = 'http://localhost:3000/photos'
-    Ω(file)
-    request
-      .post(serverroot)
-      .type('form')
-      .send(file)
-      .end(function(err, res) {
-        if (err) {
-          console.log(err)
-        }
-      // console.log('res', res)
-      })
-  },
 
   render () {
     return (
       <div>
         <Navbar />
         <Grid className='fluid-container'>
-          {React.cloneElement(this.props.children, {uploadPhoto: this.uploadPhoto}) || <Feed/>}
+          {React.cloneElement(this.props.children) || <Feed/>}
         </Grid>
       </div>
     )
--- src/modules/Upload.js
+++ src/modules/Upload.js
@@ -1,24 +1,11 @@
 import React from 'react'
-var FileInput = require('react-file-input')
 
 export default React.createClass({
-
-  handleChange: function (event) {
-    this.props.uploadPhoto(event.target.files[0])
-  },
 
   render: function () {
     return (
       <div>
-        <h1>Upload a new photo</h1>
-        <form >
-          <input
-            name="file"
-            type="file"
-            className="cloudinary-fileupload" data-cloudinary-field="image_id"
-            onChange={this.handleChange}>
-          </input>
-        </form>
+        <a href='#' id='upload_widget_opener'></a>
       </div>
     )
   }
