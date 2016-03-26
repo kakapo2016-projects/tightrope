32279fbf5617d519d5fc5585d4d3090f133f0620
Howard Smith <howardsmithnz@gmail.com>
2016-03-26 23:43:29 +1300

    improved the API routes a bit

---
+++ src/getrequest.js
@@ -0,0 +1,9 @@
+var request = require('superagent')
+
+module.exports = function getRequest (geturl, callback) {
+	request
+		.get(geturl)
+		.end(function (err, res) {
+			callback(err, res.body)
+		})
+}
--- src/modules/Login.js
+++ src/modules/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Input, ButtonInput } from 'react-bootstrap'
 import request from 'superagent'
+import { Redirect } from 'react-router'
 
 
 export default React.createClass({
@@ -8,13 +9,28 @@ export default React.createClass({
     return {}
   },
 
+  attemptLogIn: function (email, password) {
+     getRequest(`http://localhost:3000/api/v1/user${email}`, (err, res) => {
+       if (err) { console.log('ERROR: ', err); return }
+       if (res === null) { alert('you call that a valid email address, idiot?'); return }
+       /// run password through bcript
+       if (password === res.passwordHash) {
+         alert('sucessfully logged in!')
+       } else {
+         alert('incorrect password!')
+       }
+     })
+   },
+
+
   loginRequest: function () {
-    Ω('Log in request')
     request
     .get('http://localhost:3000/api/v1/login')
+    .query({email: this.state.email})
     .end( function (err, res) {
       if (err) console.log('Error:', err)
       this.setState({user: res.body})
+        Ω('---------> ', res.body)
     }.bind(this))
   },
 
