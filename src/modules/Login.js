2f3365d5edf43f311889c533e7f18bf94965637d
Ashley <Ashleystanbridge@icloul.com>
2016-03-26 22:15:25 +1300

    signup WORKING

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -34,6 +34,18 @@ export default React.createClass({
     }.bind(this))
   },
 
+  signUpRequest: function () {
+    Ω('Log in request')
+    request
+    .post('http://localhost:3000/api/v1/signup')
+    .send({email: this.state.email, username: this.state.email, password: this.state.password})
+    .end( function (err, res) {
+      if (err) console.log('Error:', err)
+      this.setState({user: res.body})
+        Ω('resbod ---------> ', res.body)
+    }.bind(this))
+  },
+
   handlePassChange (e) {
     Ω('handlePassChange')
     this.setState({password: e.target.value})
@@ -43,6 +55,24 @@ export default React.createClass({
     Ω('handleEmailChange')
     this.setState({email: e.target.value})
   },
+
+  handleUsernameChange (e) {
+      Ω('handleUsernameChange')
+    this.setState({username: e.target.value})
+  },
+
+  handleSignupSubmit: function(e) {
+    console.log('handleSignupSubmit')
+        e.preventDefault()
+        var email = this.state.email
+        var username = this.state.username
+        var password = this.state.password
+        if (!email || !password || !username) {
+          return
+        }
+        this.signUpRequest({email: email, password: password})
+        this.setState({email: '', password: '', username: ''})
+      },
 
   handleSubmit: function(e) {
     console.log('handleSubmit')
@@ -59,11 +89,23 @@ export default React.createClass({
 
   render () {
 		return (
+    <div>
       <form onSubmit={this.handleSubmit}>
          <Input type="email" label="Email Address" placeholder="Enter email" value={this.state.email} onChange={this.handleEmailChange} />
          <Input type="password" label="Password" placeholder="Enter password" value={this.state.password} onChange={this.handlePassChange} />
-         <ButtonInput type="submit" value="post" />
+         <ButtonInput type="submit" value="submit" />
        </form>
+
+       <form onSubmit={this.handleSignupSubmit}>
+          <Input type="email" label="Email Address" placeholder="Enter email" value={this.state.email} onChange={this.handleEmailChange} />
+
+            <Input type="text" label="Username" placeholder="User Name" value={this.state.username} onChange={this.handleUsernameChange} />
+
+          <Input type="password" label="Password" placeholder="Enter password" value={this.state.password} onChange={this.handlePassChange} />
+
+          <ButtonInput type="submit" value="submit" />
+        </form>
+     </div>
 
     )
   }
