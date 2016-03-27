99030e047cc126c434a0e9ca9ee2e82377206402
Nick Maskell <nrmaskell@gmail.com>
2016-03-27 13:34:06 +1300

    Fix formatting on feed

--- src/components/Feed-photos.js
+++ src/components/Feed-photos.js
@@ -12,12 +12,12 @@ export default React.createClass({
               <div className='panel feedPhoto'>
                 <img src={photo.photo_url} className='img-responsive'/>
                 <div className='feedInfo'>
-                  <span><i className="fa fa-gratipay"> {photo.likes}</i></span>
-                  <span><i className="fa fa-comments"> {photo.comments}</i></span>
-                  <span><i className="fa fa-space-shuttle"> {photo.activeStreak}</i></span>
+                  <span className='likes'><i className="fa fa-gratipay"> {photo.likes}</i></span>
+                  <span className='comments'><i className="fa fa-comments"> {photo.comments}</i></span>
+                  <span className='streak'><i className="fa fa-space-shuttle"> {photo.activeStreak}</i></span>
                 </div>
               </div>
-              <div>{photo.username}</div>
+              <div className='username'>{photo.username}</div>
             </div>
           )
         })}
