6581b6d444e65f8f44e5b62b1466f9f9855f4231
Nick Maskell <nrmaskell@gmail.com>
2016-03-28 11:53:10 +1300

    Add real data to profile

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default React.createClass({
 
   render: function () {
+    console.log('Profile Pic: ', this.props.profilepic)
     return (
       <div>
         <img className='profilephoto' src={this.props.profilepic} />
--- src/modules/Profile.js
+++ src/modules/Profile.js
@@ -9,59 +9,54 @@ import get from '../get-request'
 require('../stylesheets/modules/profile.sass')
 
 export default React.createClass({
-  // getInitialState: function () {
-  //   return {      data: [
-  //           {
-  //             "photos": {
-  //               "photoset": []
-  //             },
-  //             "profile": {
-  //               "accolades": [
-  //                 {
-  //                   "activeStreak": 0,
-  //                   "badges": [],
-  //                   "credits": 0
-  //                 }
-  //               ],
-  //               "photoset": [],
-  //               "profilepic": "",
-  //               "username": ""
-  //             }
-  //           }
-  //         ]
-  //       }
-  // },
-  //
-  // componentDidMount: function () {
-  //   console.log('making request')
-  //   get('http://localhost:3000/api/v1/users/' + cookie.load('userId') + '/photos', '', function (err, res) {
-  //     console.log('getting response')
-  //     if (err) console.log('Error:', err)
-  //     console.log(res)
-  //     this.setState({userPhotos: res})
-  //     get('http://localhost:3000/api/v1/users/' + cookie.load('userId') + '/profile', '', function (err, resp) {
-  //       console.log('getting response for profile')
-  //       if (err) console.log('Error:', err)
-  //       console.log('Profile response: ', resp)
-  //       this.setState({profile: resp})
-  //     }.bind(this))
-  //   }.bind(this))
-  // },
+  getInitialState: function () {
+  return {
+    photos:[],
+    profile: {
+      profile_pic: '',
+      username: '',
+      badges: [],
+      credits: 0,
+      active_streak: 0,
+      username: ''
+      }
+    }
+  },
+
+  componentDidMount: function () {
+    console.log('making request')
+    get('http://localhost:3000/api/v1/users/' + cookie.load('userId') + '/photos', '', function (err, res) {
+      console.log('getting response')
+      if (err) console.log('Error:', err)
+      console.log('userPhotos: ', res)
+      this.setState({photos: res})
+    }.bind(this))
+    get('http://localhost:3000/api/v1/users/' + cookie.load('userId') + '/profile', '', function (err, resp) {
+      console.log('getting response for profile')
+      if (err) console.log('Error:', err)
+      console.log('Profile: ', resp)
+      this.setState({profile: resp})
+      console.log('This is everything: ', this.state)
+    }.bind(this))
+  },
 
   render: function () {
+
+    let { profile_pic, username } = this.state.profile
+    let { photos }  = this.state
+    let accolades = this.state.profile
+
     return (
-      <Col sm={4} className='profile panel'/>
+      <Row>
+        <Col sm={4} className='profile panel'>
+          <ProfilePic profilepic={profile_pic}/>
+          <h2 className='username'>{username}</h2>
+          <Accolades accolades={accolades}/>
+        </Col>
+        <Col sm={8} className='feed centered' className='container-fluid'>
+          <Photoset photoset={photos || []}/>
+        </Col>
+      </Row>
     )
   }
 })
-
-{/*<Row>
-  <Col sm={4} className='profile panel'>
-    <ProfilePic profilepic={this.state.data.profile}/>
-    <h2 className='username'>{this.state.data.profile}</h2>
-    <Accolades accolades={this.state.data.profile}/>
-  </Col>
-  <Col sm={8} className='feed centered' className='container-fluid'>
-    <Photoset photoset={this.state.photos}/>
-  </Col>
-</Row>*/}
