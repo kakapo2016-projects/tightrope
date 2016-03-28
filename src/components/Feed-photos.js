a5afc24f6931261156a14142b0dfe384a8fccd92
Nick Maskell <nrmaskell@gmail.com>
2016-03-29 12:16:16 +1300

    Add active streak counter to feed

--- src/components/Feed-photos.js
+++ src/components/Feed-photos.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import _ from 'lodash'
 import { Link } from 'react-router'
+import get from '../get-request-simple'
+import moment from 'moment'
 
 export default React.createClass({
+
 
   render: function () {
     return (
       <div className='photoset'>
         {_.map(this.props.photos, function (photo) {
+          var diff = moment().diff(photo.created_at, 'days')
           let routeID = '/photos/' + photo.photo_id
           return (
             <div>
@@ -17,14 +21,14 @@ export default React.createClass({
                   <div className='feedInfo'>
                     <span className='likes'><i className='fa fa-star'> {photo.likes}</i></span>
                     <span className='comments'><i className='fa fa-comment'> {photo.comments}</i></span>
-                    <span className='streak'><i className='fa fa-fire'> {photo.activeStreak}</i></span>
+                    <span className='streak'><i className='fa fa-fire'> {diff}</i></span>
                   </div>
                 </div>
               </Link>
               <div className='username'>{photo.username}</div>
             </div>
           )
-        })}
+        }.bind(this))}
       </div>
     )
   }
