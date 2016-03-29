2fc901aa1a7e43453da58148e01f6d29f7ac4db3
Nick Maskell <nrmaskell@gmail.com>
2016-03-29 14:37:11 +1300

    add username to photo

---
+++ src/modules/Foreign-profile.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import request from 'superagent'
+import cookie from 'react-cookie'
+import ProfilePic from '../components/ProfilePic'
+import Photoset from '../components/Photoset'
+import Accolades from '../components/Accolades'
+import { Col, Row } from 'react-bootstrap'
+import get from '../get-request'
+require('../stylesheets/modules/profile.sass')
+
+export default React.createClass({
+
+  getInitialState: function () {
+    return {
+      foreignId: 0,
+      photos: [],
+      profile: {
+        profile_pic: '',
+        username: '',
+        badges: [],
+        credits: 0,
+        active_streak: 0
+      }
+    }
+  },
+
+  componentDidMount: function () {
+    get('http://localhost:3000/api/v1/users/' + this.state.foreignId + '/photos', '', function (err, res) {
+      if (err) console.log('Error:', err)
+      this.setState({photos: res})
+    }.bind(this))
+    get('http://localhost:3000/api/v1/users/' + this.state.foreignId + '/profile', '', function (err, resp) {
+      if (err) console.log('Error:', err)
+      this.setState({profile: resp})
+    }.bind(this))
+  },
+
+  render: function () {
+    let { profile_pic, username } = this.state.profile
+    let { photos } = this.state
+    let accolades = this.state.profile
+
+    return (
+      <Row>
+        <Col sm={4} className='profile panel'>
+          <ProfilePic profilePic={profile_pic}/>
+          <h2 className='username'>{username}</h2>
+          <Accolades accolades={accolades}/>
+        </Col>
+        <Col sm={8} className='feed centered' className='container-fluid'>
+          <Photoset photoset={photos || []}/>
+        </Col>
+      </Row>
+    )
+  }
+})
