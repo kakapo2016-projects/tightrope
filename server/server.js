833411b13e97a3590e5f03c526de04a02e059f51
Nick Maskell <nrmaskell@gmail.com>
2016-03-30 20:58:54 +1300

    Sending to Anna

--- server/server.js
+++ server/server.js
@@ -21,21 +21,7 @@ require('./get-routes')(app, cors, corsOptions)
 require('./post-routes')(app, cors, corsOptions)
 require('./authentication-routes')(app, cors, corsOptions)
 
-  // ----- set up DB ----- //
-
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+  // ----- set up DB ----- /
 
 app.use(knex({
   client: 'pg',
