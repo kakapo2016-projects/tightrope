479f7d2f41e8f754941cfd895ebddc2930ecb1e3
Nick Maskell <nrmaskell@gmail.com>
2016-03-30 19:45:52 +1300

    Delete database

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(knex({
   connection: {
     host     : process.env.DBHOST,
     port     : '5432',
-    database : process.env.DBNAME || database,
+    database : process.env.DBNAME,
     user:     process.env.DBUSER,
     password: process.env.DBPASSWORD
   },
