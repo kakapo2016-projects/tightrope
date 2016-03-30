9a67b3c56bc89f77e4418ff0c55ab39386a81270
Anna Blackwell <annablackwell44@gmail.com>
2016-03-30 15:05:04 +1300

    Revert "Seeding data"

--- seeds/badges.js
+++ seeds/badges.js
@@ -2,7 +2,7 @@ var badge = require('../data/seed-badges')
 
 exports.seed = function (knex, Promise) {
   var badgePromises = []
-  badge.forEach(function (badge) {
+  badge.forEach(function (donor) {
     badgePromises.push(createBadge(knex, badge))
   })
   return Promise.all(badgePromises)
