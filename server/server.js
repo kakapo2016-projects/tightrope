20d058000ec31e2ef4b6d3802c6c9467b26bf924
Nick Maskell <nrmaskell@gmail.com>
2016-03-30 20:50:23 +1300

    Fix

--- server/server.js
+++ server/server.js
@@ -23,6 +23,20 @@ require('./authentication-routes')(app, cors, corsOptions)
 
   // ----- set up DB ----- //
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 app.use(knex({
   client: 'pg',
   connection: {
