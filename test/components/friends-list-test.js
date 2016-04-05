f1262de4d3a29960d5f706ebfe336cf5b77b4ea1
Howard Smith <howardsmithnz@gmail.com>
2016-04-05 17:37:35 +1200

    fix of some typos

--- test/components/friends-list-test.js
+++ test/components/friends-list-test.js
@@ -1,4 +1,4 @@
-import FriendsList from '../../src/components/Friends-list'
+import FriendsList from '../../src/components/friends-list'
 import { expect } from 'chai'
 import { shallow, render, mount } from 'enzyme'
 import React from 'react'
@@ -6,20 +6,20 @@ import React from 'react'
 describe('FriendsList', () => {
 
   const props = { friends: [
-    {
-      username: 'a',
-      profile_pic: 'a.jpg',
-      likes: 4,
-      streak: 5,
-    },
-    {
-      username: 'b',
-      profile_pic: 'b.jpg',
-      likes: 5,
-      streak: 6,
-    }
-  ]
-}
+      {
+        username: 'a',
+        profile_pic: 'a.jpg',
+        likes: 4,
+        streak: 5,
+      },
+      {
+        username: 'b',
+        profile_pic: 'b.jpg',
+        likes: 5,
+        streak: 6,
+      }
+    ]
+  }
 
   it('should have a div with class of friends', () => {
     const wrapper = shallow(<FriendsList />)
